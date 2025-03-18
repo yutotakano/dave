32049efd864308c67d495fb718c324a2b7bd1a54
Yuto Takano <moa17stock@gmail.com>
2025-03-18 18:33:33 +0100

    Add DaveSignatureVersion to Dave.Types

--- src/Dave/Types.hs
+++ src/Dave/Types.hs
@@ -6,7 +6,7 @@
 module Dave.Types where
 
 import Foreign.Ptr ( Ptr )
-import Data.Word ( Word16 )
+import Data.Word ( Word8, Word16 )
 
 import Language.C.Inline qualified as C
 import Language.C.Types qualified as CT
@@ -19,16 +19,26 @@ data RawDaveMLSSession
 -- on it to initialise it.
 type MLSSession = Ptr RawDaveMLSSession
 
--- | Dave protocol version is represented as a uint16_t in libdave, but this is
--- an implementation detail so we also hide it behind a wrapper type in the
--- Haskell API too.
+-- | A type representing the protocol version for DAVE.
+--
+-- Internally, libdave (in C++) represents this version as a uint16_t, so our
+-- Haskell library also stores it as a 'Word16'. However, as this is an
+-- implementation detail, we've hid it behind the newtype wrapper in the API.
 newtype DaveProtocolVersion = DaveProtocolVersion Word16
     deriving (Show, Read, Eq) via Word16
 
+-- | A type representing the signature version for DAVE.
+--
+-- Internally, libdave (in C++) represents this version as a uint8_t, so our
+-- Haskell library also stores it as a 'Word8'. However, as this is an
+-- implementation detail, we've hid it behind the newtype wrapper in the API.
+newtype DaveSignatureVersion = DaveSignatureVersion Word8
+    deriving (Show, Read, Eq) via Word8
 
 -- | Set up matching types between C++ return types and Haskell types
 daveContext :: C.Context
 daveContext = Cpp.cppTypePairs
     [ ( "mls::Session" :: CT.CIdentifier, [t| RawDaveMLSSession |] )
     , ( "ProtocolVersion" :: CT.CIdentifier, [t| Word16 |])
+    , ( "SignatureVersion" :: CT.CIdentifier, [t| Word8 |])
     ]
